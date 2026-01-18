57a96113c5cee8c4f6b0fe5b93643853f1e24e6b
omkar <omkarjadhav095@gmail.com>
2026-01-18 18:00:04 +0530

    Update CORS to allow all origins

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5174',
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
